4c58102cad2ae459496b55289f5b53fcdf87636d
Mattherw <officialmatthughes>
2022-05-08 20:21:00 -0700

    added js script to show and hide sections

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+// I'm using a toggler to show and hide everything
+// inside the reason container id on the index file
+
+function toggleReasons() {
+    // Get my element by ID
+    var reasonContainer = document.getElementById("reason-container");
+
+    // Toggle
+    if (reasonContainer.style.display === "none") {
+        reasonContainer.style.display = "block";
+    } else {
+        reasonContainer.style.display = "none";
+    }
+}
